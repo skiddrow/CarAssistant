1de75a6d44395c91a5b8a3992b078973cc428f3b
Oleksandr Rybitskyi <orybi@softserveinc.com>
2019-03-22 14:58:03 +0200

    test endpoint

--- CarAssistant/Controllers/HomeController.cs
+++ CarAssistant/Controllers/HomeController.cs
@@ -20,6 +20,11 @@ namespace CarAssistant.Controllers
             return View();
         }
 
+        public IActionResult Test()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
